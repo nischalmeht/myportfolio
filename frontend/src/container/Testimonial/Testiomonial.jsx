aeb516aa45f9dbf6abc31e2748a968d823e24cb7
nischalmeht <58307277+nischalmeht@users.noreply.github.com>
2025-01-31 22:06:47 +0530

    first commit

---
+++ frontend/src/container/Testimonial/Testiomonial.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ const Testiomonial = () => {
+  return (
+    <div>Testiomonial</div>
+  )
+}
+export default Testiomonial
---
+++ frontend/src/container/index.js
@@ -0,0 +1,15 @@
+import About from './About/About';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Skill from './Skills/Skill';
+import Testimonial from './Testimonial/Testiomonial';
+import Work from './Work/Work';
+
+export {
+  About,
+  Footer,
+  Header,
+  Skill,
+  Testimonial,
+  Work,
+};
