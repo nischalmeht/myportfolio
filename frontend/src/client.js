bbd5fc2280f763350f591bfc4c655b34462d5fce
nischalmeht <58307277+nischalmeht@users.noreply.github.com>
2025-02-09 23:25:32 +0530

    fix

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -1,15 +1,30 @@
+// import sanityClient from '@sanity/client';
+// import imageUrlBuilder from '@sanity/image-url';
+// import { createClient } from '@sanity/client';
+
+// // require('dotenv').config()
+// export const client = createClient({
+//   projectId: import.meta.env.PROJECT_ID,
+//   dataset: 'production',
+//   apiVersion: '2022-02-01',
+//   useCdn: true,
+//   token: import.meta.env.REACT_APP_SANITY_TOKEN,
+// });
+// const builder= imageUrlBuilder(client);
+
+// export const urlFor=(source)=>builder.image(source);
+
 import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 import { createClient } from '@sanity/client';
 
-// require('dotenv').config()
 export const client = createClient({
-  projectId: 'hu87rtj3',
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token: import.meta.env.REACT_APP_SANITY_TOKEN,
+  token: import.meta.env.VITE_SANITY_TOKEN, // Ensure this is properly set
 });
 const builder= imageUrlBuilder(client);
 
-export const urlFor=(source)=>builder.image(source);
+export const urlFor = (source) => builder.image(source);
--- frontend/src/container/Skills/Skill.jsx
+++ frontend/src/container/Skills/Skill.jsx
@@ -1,10 +1,51 @@
 
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+// import ReactTooltip from react-tooltip;
+import { AppWrapper,} from '../wrapper';
+import { urlFor, client } from '../../client';
 import './Skill.scss'
-import React from 'react'
-
 const Skill = () => {
+    const [experiences, setExperiences] = useState([]);
+    const [skills, setSkills] = useState([]);
+  useEffect(() => {
+      const query = '*[_type == "experiences"]';
+      const skillsquery = '*[_type == "skills"]';
+  
+      client.fetch(query).then((data) => {
+        setExperiences(data);     
+      });
+      client.fetch(skillsquery).then((data) => {
+        
+        setSkills(data);
+      });
+    },
+   
+  
+    []);
   return (
-    <div>Skill</div>
+   <>
+    <h2 className="head-text">Skills and Experience</h2>
+    <motion.div className="app__skills-list">
+          {skills.map((skill) => (
+            <motion.div
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skills-item app__flex"
+              key={skill.name}
+            >
+              <div
+                className="app__flex"
+                style={{ backgroundColor: skill.bgColor }}
+              >
+                <img src={urlFor(skill.icon)} alt={skill.name} />
+              </div>
+              <p className="p-text">{skill.name}</p>
+            </motion.div>
+          ))}
+        </motion.div>
+   </>
+
   )
 }
 
